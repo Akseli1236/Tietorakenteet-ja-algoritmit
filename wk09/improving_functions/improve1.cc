584bb19d3231386d3d95397bb0b6ddc4296ce769
tbakah <akseli.ahonen@tuni.fi>
2023-11-03 23:52:56 +0200

    mistakes were made

--- wk09/improving_functions/improve1.cc
+++ wk09/improving_functions/improve1.cc
@@ -13,21 +13,22 @@
  * @return std::vector<int>
  */
 std::vector<int> ascendingVector(int n){
-    time_t start, end;
+    //time_t start, end;
     std::vector<int> v(n);
-    std::iota (v.begin(), v.end(), 1);
+    std::iota (v.begin(), v.end(), 0);
 
 
-    time(&start);
+    //time(&start);
     /*
     for ( int i = 0; i < n ; i++){
         v.insert(v.end(), i);
     }
-*/
+
     time(&end);
     double time_taken = double(end - start);
     std::cout << "Time taken by program is : " << std::fixed
          << time_taken << std::setprecision(5);
     std::cout << " sec " << std::endl;
+*/
     return v;
 }
